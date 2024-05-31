fc81433dc668a78958921cd7b0f46aafc3873b2a
sandeep7567 <sandeepthakur7567@gmail.com>
2024-05-31 20:47:50 +0530

    add usePreventDevTools

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,8 +1,10 @@
 import { useFetchCartItems } from "@/hooks/store/useFetchCarItems";
+import { usePreventDevTools } from "@/hooks/ui/usePreventDevtools";
 import ModalProvider from "@/providers/ModalProvider";
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
+  usePreventDevTools();
   useFetchCartItems();
   return (
     <>
